50c97ca9898f03ccfc47bdb520ac3790a571612b
Dylan Burns <notdylanburns@gmail.com>
2021-06-29 21:38:35 +0100

    Initial commit

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
 
-const PORT = 8081;
+const PORT = 8082;
 
 app.get("/", (req, res) => {
     res.sendFile(`${__dirname}/index.html`);
 });
+
+app.get("/static/img", (req, res) => {
+    res.sendFile(`${__dirname}/conscalc.png`);
+})
 
 app.listen(PORT, () => console.log(`Running on *:${PORT}`));
